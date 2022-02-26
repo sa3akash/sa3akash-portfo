79a54411332737e775271cfea77bab00feacac1d
sa3akash <shakilfb626@gmail.com>
2022-02-26 13:42:57 -0800

    change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styled from "styled-components";
-import Circle from "./Circle";
 import { HomeSocial } from "./HomeSocial";
 
 const Home = () => {
   return (
-    <Container className="home section bd_grid">
-      <Circle /> {/*circle render*/}
+    <Container className="home section bd_grid" id="home">
+      <Circle1 className="circle"></Circle1>
+      <Circle2 className="circle"></Circle2>
+      <Circle3 className="circle"></Circle3>
       <HomeData>
         <h2>
           Hi , I'am <br /> <span>Shakil Ahmed</span> <br /> Web Developer
@@ -78,6 +79,27 @@ const HomeData = styled.div`
       }
     }
   }
+`;
+const Circle1 = styled.div`
+  width: 300px;
+  height: 300px;
+  background: var(--circle-one);
+  top: 60%;
+  left: 0;
+`;
+const Circle2 = styled.div`
+  width: 250px;
+  height: 250px;
+  background: var(--circle-two);
+  top: 10%;
+  right: 0;
+`;
+const Circle3 = styled.div`
+  width: 200px;
+  height: 200px;
+  background: var(--circle-two);
+  top: 30%;
+  left: 0;
 `;
 
 const ContactButton = styled.a`
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,15 +1,41 @@
 import React from "react";
 import styled from "styled-components";
-import Circle from "./Circle";
+
 
 const Services = () => {
   return (
-    <ServicesContainer className="section bd_grid">
-      <Circle />
+    <ServicesContainer className="section bd_grid" id="services">
+     <Circle1 className="circle"></Circle1>
+      <Circle2 className="circle"></Circle2>
+      <Circle3 className="circle"></Circle3>
     </ServicesContainer>
   );
 };
 
 export default Services;
 
-const ServicesContainer = styled.div``;
+const ServicesContainer = styled.div`
+    height: 100vh;
+`;
+const Circle1 = styled.div`
+  width: 300px;
+  height: 300px;
+  background: var(--circle-one);
+  top: 60%;
+  left: 0;
+`;
+const Circle2 = styled.div`
+  width: 250px;
+  height: 250px;
+  background: var(--circle-two);
+  top: 10%;
+  right: 0;
+`;
+const Circle3 = styled.div`
+  width: 200px;
+  height: 200px;
+  background: var(--circle-two);
+  top: 30%;
+  left: 0;
+`;
+
