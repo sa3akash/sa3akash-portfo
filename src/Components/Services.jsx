99a93d84ee7bdd8296eef5efd979103d3e3af398
sa3akash <shakilfb626@gmail.com>
2022-02-26 13:46:51 -0800

    change

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -7,7 +7,6 @@ const Services = () => {
     <ServicesContainer className="section bd_grid" id="services">
      <Circle1 className="circle"></Circle1>
       <Circle2 className="circle"></Circle2>
-      <Circle3 className="circle"></Circle3>
     </ServicesContainer>
   );
 };
@@ -18,24 +17,17 @@ const ServicesContainer = styled.div`
     height: 100vh;
 `;
 const Circle1 = styled.div`
-  width: 300px;
-  height: 300px;
-  background: var(--circle-one);
-  top: 60%;
-  left: 0;
-`;
-const Circle2 = styled.div`
   width: 250px;
   height: 250px;
-  background: var(--circle-two);
-  top: 10%;
+  background: var(--circle-one);
+  top: 0;
   right: 0;
 `;
-const Circle3 = styled.div`
+const Circle2 = styled.div`
   width: 200px;
   height: 200px;
   background: var(--circle-two);
-  top: 30%;
   left: 0;
+  bottom: 0;
 `;
 
